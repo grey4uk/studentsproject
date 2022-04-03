45b0a08ac087d4ae08834f72e010a04cb57e448a
grey4uk <grey4uk14@gmail.com>
2022-04-03 16:47:32 +0300

    changed connection of styles in project

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './sass/main.scss';
+// import './sass/main.scss';
 import fetchApiGet from './fetchApiGet';
 import fetchNewEvents from './newArrayAndGetModal';
 import code from './countries.json';
